1f9da1433b483203bf897a510afc2c21cd53c610
AinaArd <ainaard@mail.ru>
2019-03-22 08:48:59 +0800

    added tasksService

--- src/main/java/screens/TasksScreen.java
+++ src/main/java/screens/TasksScreen.java
@@ -2,13 +2,17 @@ package screens;
 
 import dao.TasksDao;
 import models.Card;
+import models.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import services.TasksService;
 import utils.ScannerFactory;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class TasksScreen {
     private Scanner scanner;
+    private List<Task> userTasks;
 
     @Autowired
     private TasksDao tasksDao;
